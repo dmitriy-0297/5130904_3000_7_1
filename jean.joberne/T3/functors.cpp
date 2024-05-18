53879cd9a89b730baf56e321898633853a9e8f17
Jorbyart24 <jjorby00@gmail.com>
2024-05-18 14:23:56 +0300

    T3

--- jean.joberne/T3/functors.cpp
+++ jean.joberne/T3/functors.cpp
@@ -1,22 +1,17 @@
 #include "functors.h"
-#include "geometry.h"
-#include "io.h"
+#include <iostream>
 #include <algorithm>
+#include <functional>
+#include <numeric>
+#include <vector>
 #include <iterator>
 #include <stdexcept>
-#include <iostream>
-#include <numeric>
-#include <functional>
-#include <vector>
-#include <string>
-#include <cmath>
 
-using namespace jean;
 using namespace std::placeholders;
-// Function to get the total area of polygons based on criteria
-void functors::getTotalArea(const std::vector<jean::Polygon>& data)
+
+void jean::area(const std::vector<Polygon> &data)
 {
-    auto accumulateAreaIf = [](double ac, const jean::Polygon& poly, std::size_t mod2, std::size_t vertices)
+    auto accumulateArea_if = [](double ac, const jean::Polygon &poly, std::size_t mod2, std::size_t vertices)
     {
         if ((poly.points.size() % 2 == mod2) || (mod2 == 2 && poly.points.size() == vertices) || (mod2 == 3))
         {
@@ -30,30 +25,30 @@ void functors::getTotalArea(const std::vector<jean::Polygon>& data)
     if (arg == "EVEN")
     {
         std::cout << std::accumulate(data.begin(), data.end(), 0.0,
-                    std::bind(accumulateAreaIf, _1, _2, 0, 0)) << '\n';
+                                     std::bind(accumulateArea_if, _1, _2, 0, 0)) << '\n';
     }
     else if (arg == "ODD")
     {
         std::cout << std::accumulate(data.begin(), data.end(), 0.0,
-                    std::bind(accumulateAreaIf, _1, _2, 1, 0)) << '\n';
+                                     std::bind(accumulateArea_if, _1, _2, 1, 0)) << '\n';
     }
     else if (arg == "MEAN" && data.size() != 0)
     {
         std::cout << std::accumulate(data.begin(), data.end(), 0.0,
-                     std::bind(accumulateAreaIf, _1, _2, 3, 0)) / data.size() << std::endl;
+                                     std::bind(accumulateArea_if, _1, _2, 3, 0)) / data.size() << std::endl;
     }
     else if (std::all_of(arg.begin(), arg.end(), isdigit) && stoi(arg) > 2)
     {
         std::cout << std::accumulate(data.begin(), data.end(), 0.0,
-                    std::bind(accumulateAreaIf, _1, _2, 2, stoi(arg))) << '\n';
+                                     std::bind(accumulateArea_if, _1, _2, 2, stoi(arg))) << '\n';
     }
     else
     {
         throw std::runtime_error("<INVALID COMMAND>");
     }
 }
-// Function to get the minimum area or vertex count of polygons
-void functors::getMin(const std::vector<jean::Polygon>& data)
+
+void jean::min(const std::vector<Polygon> &data)
 {
     if (data.empty())
         throw std::runtime_error("<INVALID COMMAND>");
@@ -67,19 +62,24 @@ void functors::getMin(const std::vector<jean::Polygon>& data)
     }
     else if (arg == "VERTEXES")
     {
-        std::cout << std::min_element(data.begin(), data.end(), 
-            [](const jean::Polygon& a, const jean::Polygon& b)
-            {
-                return a.points.size() < b.points.size();
-            })->points.size() << '\n';
+        std::vector<std::size_t> sizes(data.size());
+
+        std::cout << std::accumulate(
+                         data.begin() + 1,
+                         data.end(),
+                         data[0].points.size(),
+                         [](std::size_t min, const Polygon &poly)
+                         {
+                             return (poly.points.size() < min ? poly.points.size() : min);
+                         }) << '\n';
     }
     else
     {
         throw std::runtime_error("<INVALID COMMAND>");
     }
 }
-// Function to get the maximum area or vertex count of polygons
-void functors::getMax(const std::vector<jean::Polygon>& data)
+
+void jean::max(const std::vector<Polygon> &data)
 {
     if (data.empty())
         throw std::runtime_error("<INVALID COMMAND>");
@@ -93,11 +93,16 @@ void functors::getMax(const std::vector<jean::Polygon>& data)
     }
     else if (arg == "VERTEXES")
     {
-        std::cout << std::max_element(data.begin(), data.end(),
-            [](const jean::Polygon& a, const jean::Polygon& b)
-            {
-                return a.points.size() < b.points.size();
-            })->points.size() << '\n';
+        std::vector<std::size_t> sizes(data.size());
+
+        std::cout << std::accumulate(
+                         data.begin() + 1,
+                         data.end(),
+                         data[0].points.size(),
+                         [](std::size_t max, const Polygon &poly)
+                         {
+                             return (poly.points.size() > max ? poly.points.size() : max);
+                         }) << '\n';
     }
     else
     {
@@ -105,10 +110,9 @@ void functors::getMax(const std::vector<jean::Polygon>& data)
     }
 }
 
-// Function to count polygons based on criteria
-void functors::getQuantity(const std::vector<jean::Polygon>& data)
+void jean::count(const std::vector<Polygon> &data)
 {
-    auto countPolygonsIf = [](const jean::Polygon& poly, std::size_t mod2, std::size_t vertices)
+    auto countPolygons_if = [](const jean::Polygon &poly, std::size_t mod2, std::size_t vertices)
     {
         return ((poly.points.size() % 2 == mod2) || (mod2 == 2 && poly.points.size() == vertices));
     };
@@ -118,17 +122,17 @@ void functors::getQuantity(const std::vector<jean::Polygon>& data)
     if (arg == "EVEN")
     {
         std::cout << std::count_if(data.begin(), data.end(),
-                    std::bind(countPolygonsIf, _1, 0, 0)) << '\n';
+                                   std::bind(countPolygons_if, _1, 0, 0)) << '\n';
     }
     else if (arg == "ODD")
     {
         std::cout << std::count_if(data.begin(), data.end(),
-                    std::bind(countPolygonsIf, _1, 1, 0)) << '\n';
+                                   std::bind(countPolygons_if, _1, 1, 0)) << '\n';
     }
     else if (std::all_of(arg.begin(), arg.end(), isdigit) && stoi(arg) > 2)
     {
         std::cout << std::count_if(data.begin(), data.end(),
-                    std::bind(countPolygonsIf, _1, 2, stoi(arg))) << '\n';
+                                   std::bind(countPolygons_if, _1, 2, stoi(arg))) << '\n';
     }
     else
     {
@@ -136,43 +140,16 @@ void functors::getQuantity(const std::vector<jean::Polygon>& data)
     }
 }
 
-// Function to count polygons with area less than a given polygon
-void functors::lessArea(const std::vector<jean::Polygon>& data)
-{
-    jean::Polygon basic;
-    std::cin >> basic;
-    if (std::cin.fail() || std::cin.get() != '\n')
-        throw std::runtime_error("<INVALID COMMAND>");
-
-    std::cout << std::count_if(data.begin(), data.end(),
-        [&basic](const jean::Polygon& poly)
-        {
-            return poly.getArea() < basic.getArea();
-        }) << '\n';
-}
-
-// Function to count polygons that are identical to a given polygon
-void functors::same(const std::vector<jean::Polygon>& data)
-{
-    jean::Polygon basic;
-    std::cin >> basic;
-    if (std::cin.fail() || std::cin.get() != '\n')
-        throw std::runtime_error("<INVALID COMMAND>");
-
-    std::cout << std::count(data.begin(), data.end(), basic) << '\n';
-}
-
-// Function to duplicate occurrences of a given polygon
-void functors::echo(std::vector<jean::Polygon>& data)
+void jean::echo(std::vector<Polygon> &data)
 {
     jean::Polygon poly;
     std::cin >> poly;
     if (std::cin.fail() || std::cin.get() != '\n')
         throw std::runtime_error("<INVALID COMMAND>");
 
-    std::vector<jean::Polygon> result;
+    std::vector<Polygon> result;
     int count = 0;
-    for (const jean::Polygon& el : data)
+    for (const Polygon &el : data)
     {
         result.push_back(el);
         if (el == poly)
@@ -181,7 +158,19 @@ void functors::echo(std::vector<jean::Polygon>& data)
             result.push_back(el);
         }
     }
-    data.swap(result);
+    data.resize(result.size());
+    copy(result.begin(), result.end(), data.begin());
     std::cout << count << '\n';
+}
+
+void jean::inframe(const std::vector<Polygon> &data)
+{
+    jean::Polygon poly;
+    std::cin >> poly;
+    if (std::cin.fail() || std::cin.get() != '\n')
+        throw std::runtime_error("<INVALID COMMAND>");
+
+    jean::Frame frame = getFrame(data);
+    std::cout << (frame.containsPolygon(poly) ? "<TRUE>" : "<FALSE>") << '\n';
 }
 
